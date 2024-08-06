c8ca78a26f2b0afa8e12cae645210bf26de76cc7
sattikb <sattikb@gmail.com>
2024-08-06 11:34:38 -0500

    laminar RAS and LES

--- src/MomentumTransportModels/momentumTransportModels/laminar/Stokes/Stokes.C
+++ src/MomentumTransportModels/momentumTransportModels/laminar/Stokes/Stokes.C
@@ -111,5 +111,3 @@ void Stokes<BasicMomentumTransportModel>::correct()
 
 } // End namespace laminarModels
 } // End namespace Foam
-
-// ************************************************************************* //
